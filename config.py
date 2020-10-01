94f2eb87adad854dc40a79ae014f7ab6e3c5d011
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-01 12:56:44 +0300

    changed the size of images

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    SUBJECT_PREFIX = 'Watchlist'
+    SUBJECT_PREFIX = 'Recipe'
     SENDER_EMAIL = 'annaadhiambo01@gmail.com'
 
     # simple mde  configurations
